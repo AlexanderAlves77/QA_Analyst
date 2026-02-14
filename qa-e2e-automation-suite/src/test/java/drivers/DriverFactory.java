33de841e58898a1de479a3a7ceff5a78dc38b783
Alexander Alves <vendas77.rj@gmail.com>
2026-02-14 15:40:13 -0300

    driver: add DriverFactory for WebDriver instance creation

---
+++ qa-e2e-automation-suite/src/test/java/drivers/DriverFactory.java
@@ -0,0 +1,16 @@
+package drivers;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class DriverFactory 
+{
+	public static WebDriver getDriver() 
+	{
+		WebDriverManager.chromedriver().setup();
+		return new ChromeDriver();
+	}
+}
