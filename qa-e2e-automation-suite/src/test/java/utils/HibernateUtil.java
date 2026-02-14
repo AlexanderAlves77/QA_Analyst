fe026f843b4d7fffa3edf8cdc1dc7153ec00762a
Alexander Alves <vendas77.rj@gmail.com>
2026-02-14 15:43:18 -0300

    database: add Hibernate utility for SessionFactory management

---
+++ qa-e2e-automation-suite/src/test/java/utils/HibernateUtil.java
@@ -0,0 +1,28 @@
+package utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil 
+{
+	private static SessionFactory sessionFactory;
+	
+	static
+	{
+		try
+		{
+			sessionFactory = new Configuration()
+					.configure("hibernate.cfg.xml")
+					.buildSessionFactory();
+		}
+		catch (Exception ex)
+		{
+			throw new RuntimeException("Hibernate error: " + ex);
+		}
+	}
+	
+	public static SessionFactory getSessionFactory() 
+	{
+		return sessionFactory;
+	}
+}
