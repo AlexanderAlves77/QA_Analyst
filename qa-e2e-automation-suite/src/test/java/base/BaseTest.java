93ae137866ed2918c1f0c3c93e7634d913be0a6b
Alexander Alves <vendas77.rj@gmail.com>
2026-02-14 15:41:12 -0300

    test-base: add BaseTest class with setup and teardown methods

---
+++ qa-e2e-automation-suite/src/test/java/base/BaseTest.java
@@ -0,0 +1,27 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import drivers.DriverFactory;
+
+
+public class BaseTest 
+{
+	protected WebDriver driver;
+	
+	@BeforeMethod
+	public void setup() 
+	{
+		driver = DriverFactory.getDriver();
+		driver.manage().window().maximize();
+	}
+	
+	@AfterMethod 
+	public void teardown() 
+	{
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+}
