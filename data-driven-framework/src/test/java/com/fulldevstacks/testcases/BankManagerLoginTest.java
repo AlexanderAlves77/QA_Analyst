949ade46f60b7846077f711573d34266199d23ea
Alexander Alves <vendas77.rj@gmail.com>
2026-02-18 14:20:44 -0300

    test(bankmanager): successfully implement Bank Manager Login test

--- data-driven-framework/src/test/java/com/fulldevstacks/testcases/BankManagerLoginTest.java
+++ data-driven-framework/src/test/java/com/fulldevstacks/testcases/BankManagerLoginTest.java
@@ -1,8 +1,10 @@
 package com.fulldevstacks.testcases;
 
-import java.util.NoSuchElementException;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 import com.fulldevstacks.base.TestBase;
 
@@ -11,23 +13,56 @@ public class BankManagerLoginTest extends TestBase
 	@Test
 	public void loginAsBankManager() throws InterruptedException 
 	{
-		try {
-			log.info("Clicking on Bank Manager Login button");
-			String locator = OR.getProperty("bmlBtn");	
-			driver.findElement(By.cssSelector(locator)).click();
-			
-			// Check if the Add Customer button appears (login successful)
-			Assert.assertTrue(isElementPresent(By.cssSelector(OR.getProperty("addcustbtn"))), "Login not successfully");
-			log.info("Login successfully executed");
-			
-		} catch (NoSuchElementException e) {
-			log.error("Element not found: " + e.getMessage());
-			throw e;
-		} catch (Exception e) {
-			log.error("Test failed: " + e.getMessage());
-			throw e;
-		}
-	}
-	
-	
+		log.info("========== INÍCIO DO TESTE ==========");
+        
+        Assert.assertNotNull(driver, "Driver não foi inicializado!");
+        Assert.assertNotNull(OR, "OR Properties não foi carregado!");
+        
+        // Aguardar Angular carregar
+        waitForAngular();
+        
+        // Login como Bank Manager
+        String bankmanagerbtn = OR.getProperty("bankmanagerbtn");
+        log.info("Clicando no botão Bank Manager Login: " + bankmanagerbtn);
+        
+        // Aguardar elemento estar clicável
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(bankmanagerbtn)));
+        driver.findElement(By.cssSelector(bankmanagerbtn)).click();
+        
+        // Aguardar navegação do Angular
+        Thread.sleep(3000);
+        waitForAngular();
+        
+        // Verificar se o botão Add Customer aparece
+        String addCustLocator = OR.getProperty("addcustbtn");
+        log.info("Verificando botão Add Customer: " + addCustLocator);
+        
+        // Aguardar elemento aparecer
+        try {
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(addCustLocator)));
+            log.info("Botão Add Customer encontrado!");
+        } catch (Exception e) {
+            log.error("Botão Add Customer NÃO encontrado!");
+            
+            // Debug - listar todos os botões
+            log.info("===== TODOS OS BOTÕES NA PÁGINA =====");
+            driver.findElements(By.cssSelector("button")).forEach(btn -> {
+                log.info("Botão: " + btn.getText() + 
+                        " - Visível: " + btn.isDisplayed() +
+                        " - Class: " + btn.getAttribute("class") +
+                        " - ng-click: " + btn.getAttribute("ng-click"));
+            });
+            
+            // Debug - HTML da página
+            String pageSource = driver.getPageSource();
+            log.debug("Page source snippet: " + pageSource.substring(0, 500));
+        }
+        
+        boolean isAddCustPresent = isElementPresent(By.cssSelector(addCustLocator));
+        Assert.assertTrue(isAddCustPresent, "Login não foi bem-sucedido - Botão Add Customer não encontrado!");
+        
+        log.info("Login realizado com sucesso!");
+        Reporter.log("Login realizado com sucesso!");
+        log.info("========== FIM DO TESTE ==========");
+	}	
 }
