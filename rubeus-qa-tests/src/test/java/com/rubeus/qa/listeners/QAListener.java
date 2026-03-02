1b62314ecc8d1137085a2a5eee67f492ed11ffa6
Alexander Alves <vendas77.rj@gmail.com>
2026-03-02 16:41:06 -0300

    feat(listener): add QAListener to monitor test success and failure using JUnit TestWatcher

---
+++ rubeus-qa-tests/src/test/java/com/rubeus/qa/listeners/QAListener.java
@@ -0,0 +1,20 @@
+package com.rubeus.qa.listeners;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestWatcher;
+
+
+public class QAListener implements TestWatcher
+{
+	@Override 
+	public void testSuccessful(ExtensionContext context) 
+	{
+		System.out.println("Test passed: " + context.getDisplayName());
+	}
+	
+	@Override 
+	public void testFailed(ExtensionContext context, Throwable cause) 
+	{
+		System.out.println("Test failed: " + context.getDisplayName());
+	}
+}
