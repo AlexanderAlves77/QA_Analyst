c282045194c9ab4b0194156a2237372495c5ec09
Alexander Alves <vendas77.rj@gmail.com>
2026-03-30 12:31:29 -0300

    refactor: fix TestFailureListener reference and import in BaseTest

--- qa-e2e-automation-suite/src/test/java/base/BaseTest.java
+++ qa-e2e-automation-suite/src/test/java/base/BaseTest.java
@@ -3,9 +3,11 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import drivers.DriverFactory;
+import listeners.TestFailureListener;
 
-
+@Listeners(TestFailureListener.class)
 public class BaseTest 
 {
 	protected WebDriver driver;
