b193fa2c4f48abaff5baed911c8512e364048730
Alexander Alves <vendas77.rj@gmail.com>
2026-04-01 09:00:51 -0300

    Entry point for JUnit 5 to run Cucumber

---
+++ qa-test-api/src/test/java/com/fulldevstacks/api/CucumberTest.java
@@ -0,0 +1,13 @@
+package com.fulldevstacks.api;
+
+import io.cucumber.junit.platform.engine.Constants;
+import org.junit.platform.suite.api.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "com.fulldevstacks.api")
+public class CucumberTest 
+{
+
+}
