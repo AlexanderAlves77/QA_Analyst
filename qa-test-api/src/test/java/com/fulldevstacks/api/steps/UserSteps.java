2f3ab655a374ec830ea7f8cc72983799e0bd2fc1
Alexander Alves <vendas77.rj@gmail.com>
2026-04-01 09:18:09 -0300

    feat: implement Step Definitions for user validation using RestAssured and SpringBootTest

---
+++ qa-test-api/src/test/java/com/fulldevstacks/api/steps/UserSteps.java
@@ -0,0 +1,34 @@
+package com.fulldevstacks.api.steps;
+
+import io.cucumber.java.en.*;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class UserSteps 
+{
+	private Response response;
+	private String userPayload;
+	
+	@Given("I have a valid user payload")
+	public void i_have_a_valid_user_payload() {
+		userPayload = "{ \\\"name\\\": \\\"Alex\\\", \\\"email\\\": \\\"alex@test.com\\\" }";
+	}
+	
+	@When("I send a POST request to {string}")
+	public void i_send_a_post_request_to(String endpoint) 
+	{
+		response = RestAssured.given()
+				.contentType(ContentType.JSON)
+				.body(userPayload)
+				.post("http://localhost:8080" + endpoint);
+	}
+	
+	@Then("the status code should be 201")
+	public void the_status_code_should_be() {
+		response.then().statusCode(201);
+	}
+}
