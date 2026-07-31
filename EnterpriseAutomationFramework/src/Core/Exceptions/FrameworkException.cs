4dce045c9b898b5855fbe1d0b60e7968e3814cad
Alexander Alves <vendas77.rj@gmail.com>
2026-07-31 12:09:35 -0300

    feat(core): add base framework exception

---
+++ EnterpriseAutomationFramework/src/Core/Exceptions/FrameworkException.cs
@@ -0,0 +1,17 @@
+﻿namespace EnterpriseAutomationFramework.Core.Exceptions;
+
+public class FrameworkException : Exception
+{
+    public string ErrorCode { get; }
+
+    public FrameworkException(string message) : base(message)
+    {
+        ErrorCode = GetType().Name;
+    }
+
+    public FrameworkException(string message, Exception innerException) 
+        : base(message, innerException)
+    {
+        ErrorCode = GetType().Name;
+    }
+}
