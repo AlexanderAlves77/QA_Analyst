60fba8ce45fa04fc172c14c1237d5c75cf25c2f0
Alexander Alves <vendas77.rj@gmail.com>
2026-07-31 12:23:43 -0300

    feat(core): add result pattern

---
+++ EnterpriseAutomationFramework/src/Core/Results/Result.cs
@@ -0,0 +1,20 @@
+﻿namespace EnterpriseAutomationFramework.Core.Results;
+
+public class Result
+{
+    public bool Success { get; }
+
+    public bool Failure => !Success;
+
+    public string? Error { get; }
+
+    protected Result(bool success, string? error)
+    {
+        Success = success;
+        Error = error;
+    }
+
+    public static Result Ok() => new(true, null);
+
+    public static Result Fail(string error) => new(false, error);
+}
---
+++ EnterpriseAutomationFramework/src/Core/Results/ResultT.cs
@@ -0,0 +1,20 @@
+﻿namespace EnterpriseAutomationFramework.Core.Results;
+
+public class Result<T> : Result
+{
+    public T? Value { get; }
+
+    private Result(T value) : base(true, null)
+    {
+        this.Value = value;
+    }
+
+    private Result(string error) : base(false, error)
+    {
+        this.Value = default;
+    }
+
+    public static Result<T> Ok(T value) => new(value);
+
+    public static new Result<T> Fail(string error) => new(error);
+}
