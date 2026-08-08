1f3a0a3b7afd0fe47cc49215b56ca349507a1eb1
Alexander Alves <vendas77.rj@gmail.com>
2026-08-08 11:10:30 -0300

    feat(logging): add logging settings model

---
+++ EnterpriseAutomationFramework/src/Core/Configuration/LoggingSettings.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualBasic;
+
+namespace EnterpriseAutomationFramework.Core.Configuration;
+
+public sealed class LoggingSettings
+{
+    public const string SectionName = "Logging";
+
+    public string MinimumLevel { get; init; } = "Information";
+
+    public bool EnableConsole { get; init; } = true;
+
+    public bool EnableFile { get; init; } = true;
+
+    public string FilePath { get; init; } = "logs/eaf-.json";
+}
