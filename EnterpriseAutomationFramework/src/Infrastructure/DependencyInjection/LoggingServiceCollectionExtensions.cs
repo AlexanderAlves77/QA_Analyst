a2f20679bc0dbc882bf54984cdbe854c861eaff9
Alexander Alves <vendas77.rj@gmail.com>
2026-08-08 12:28:37 -0300

    feat(di): add logging service registration

---
+++ EnterpriseAutomationFramework/src/Infrastructure/DependencyInjection/LoggingServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using EnterpriseAutomationFramework.Core.Abstractions;
+using EnterpriseAutomationFramework.Infrastructure.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace EnterpriseAutomationFramework.Infrastructure.DependencyInjection;
+
+public static class LoggingServiceCollectionExtensions
+{
+    public static IServiceCollection AddEafLogging(this IServiceCollection services,
+        ILogger logger)
+    {
+        services.AddSingleton(logger);
+
+        services.AddSingleton<ILoggingService, SerilogLoggingService>();
+
+        return services;
+    }
+}
