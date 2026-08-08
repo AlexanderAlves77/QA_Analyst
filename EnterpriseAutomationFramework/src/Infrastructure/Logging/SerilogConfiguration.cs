39d2782db74e277c9e76053dd963afc78b1c158d
Alexander Alves <vendas77.rj@gmail.com>
2026-08-08 11:23:06 -0300

    feat(logging): add Serilog logger configuration

---
+++ EnterpriseAutomationFramework/src/Infrastructure/Logging/SerilogConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using EnterpriseAutomationFramework.Core.Configuration;
+using Serilog;
+using Serilog.Formatting.Json;
+
+namespace EnterpriseAutomationFramework.Infrastructure.Logging;
+
+public static class SerilogConfiguration
+{
+    public static ILogger CreateLogger(LoggingSettings settings)
+    {
+        var configuration = new LoggerConfiguration()
+            .MinimumLevel.Information().Enrich.FromLogContext();
+
+        if (settings.EnableConsole)
+        {
+            configuration.WriteTo.Console();
+        }
+
+        if (settings.EnableFile)
+        {
+            configuration.WriteTo.File(
+                new JsonFormatter(), 
+                settings.FilePath, 
+                rollingInterval: RollingInterval.Day
+            );
+        }
+
+        return configuration.CreateLogger();
+    }
+}
