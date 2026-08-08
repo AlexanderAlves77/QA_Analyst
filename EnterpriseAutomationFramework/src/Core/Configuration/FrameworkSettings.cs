08b5b6bfb405ae8e5951cd09fd20e5a68eacdcbe
Alexander Alves <vendas77.rj@gmail.com>
2026-08-08 09:00:36 -0300

    feat(configuration): add framework settings model

---
+++ EnterpriseAutomationFramework/src/Core/Configuration/FrameworkSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace EnterpriseAutomationFramework.Core.Configuration;
+
+public sealed class FrameworkSettings
+{
+    public const string SectionName = "Framework";
+
+    public string Environment { get; init; } = "Development";
+
+    public int DefaultTimeoutSeconds { get; init; } = 30;
+}
