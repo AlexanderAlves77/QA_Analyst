722f148f81bcd18d73585bafd1a96acd753fd976
Alexander Alves <vendas77.rj@gmail.com>
2026-08-08 12:43:24 -0300

    test(di): validate logging service resolution

---
+++ EnterpriseAutomationFramework/tests/Tests/Infrastructure/DependencyInjection/DependencyInjectionTests.cs
@@ -0,0 +1,27 @@
+﻿using EnterpriseAutomationFramework.Core.Abstractions;
+using EnterpriseAutomationFramework.Infrastructure.DependencyInjection;
+using EnterpriseAutomationFramework.Infrastructure.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace EnterpriseAutomationFramework.Tests.Infrastructure.DependencyInjection;
+
+public class DependencyInjectionTests
+{
+    [Test]
+    public void AddEafLogging_ShouldResolveLoggingService()
+    {
+        var services = new ServiceCollection();
+
+        var logger = new LoggerConfiguration().CreateLogger();
+
+        services.AddEafLogging(logger);
+
+        using var provider = services.BuildServiceProvider();
+
+        var loggingService = provider.GetService<ILoggingService>();
+
+        Assert.That(loggingService, Is.Not.Null);
+        Assert.That(loggingService, Is.TypeOf<SerilogLoggingService>());
+    }
+}
