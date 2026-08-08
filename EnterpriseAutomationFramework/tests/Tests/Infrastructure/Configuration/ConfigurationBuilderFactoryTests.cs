c9335ebb5f201802bab2eca19e3f95c77a232c76
Alexander Alves <vendas77.rj@gmail.com>
2026-08-08 15:27:07 -0300

    test(configuration): validate default development environment

---
+++ EnterpriseAutomationFramework/tests/Tests/Infrastructure/Configuration/ConfigurationBuilderFactoryTests.cs
@@ -0,0 +1,26 @@
+﻿using EnterpriseAutomationFramework.Infrastructure.Configuration;
+
+namespace EnterpriseAutomationFramework.Tests.Infrastructure.Configuration;
+
+public class ConfigurationBuilderFactoryTests
+{
+    private const string EnvironmentVariableName = "EAF_ENVIRONMENT";
+
+    [TearDown]
+    public void TearDown()
+    {
+        Environment.SetEnvironmentVariable(EnvironmentVariableName, null);
+    }
+
+    [Test]
+    public void Create_ShouldUseDevelopment_WhenEnvironmentIsNotDefined()
+    {
+        Environment.SetEnvironmentVariable(EnvironmentVariableName, null);
+
+        var configuration = ConfigurationBuilderFactory.Create();
+
+        var environment = configuration["Framework:Environment"];
+
+        Assert.That(environment, Is.EqualTo("Development"));
+    }
+}
