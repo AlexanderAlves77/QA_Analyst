f080b3a1c686f19209c7e42102fcc474e791826f
Alexander Alves <vendas77.rj@gmail.com>
2026-08-08 09:59:20 -0300

    feat(configuration): implement JSON configuration provider

---
+++ EnterpriseAutomationFramework/src/Infrastructure/Configuration/JsonConfigurationProvider.cs
@@ -0,0 +1,23 @@
+﻿using EnterpriseAutomationFramework.Core.Abstractions;
+using Microsoft.Extensions.Configuration;
+
+namespace EnterpriseAutomationFramework.Infrastructure.Configuration;
+
+public sealed class JsonConfigurationProvider
+{
+    private readonly IConfiguration _configuration;
+
+    public JsonConfigurationProvider(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public T Get<T>(string sectionName) where T : class, new()
+    {
+        var settings = new T();
+
+        _configuration.GetSection(sectionName).Bind(settings);
+
+        return settings;
+    }
+}
