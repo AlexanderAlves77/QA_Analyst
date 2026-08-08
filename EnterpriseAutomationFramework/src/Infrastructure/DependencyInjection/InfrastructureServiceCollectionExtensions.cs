e78dcb3d6c8f5607727746e8c34aacba5955cf92
Alexander Alves <vendas77.rj@gmail.com>
2026-08-08 12:31:52 -0300

    feat(di): add infrastructure service registration

---
+++ EnterpriseAutomationFramework/src/Infrastructure/DependencyInjection/InfrastructureServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using EnterpriseAutomationFramework.Tests.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace EnterpriseAutomationFramework.Infrastructure.DependencyInjection;
+
+public static class InfrastructureServiceCollectionExtensions
+{
+    public static IServiceCollection AddEafInfrastructure(
+        this IServiceCollection services, ILogger logger)
+    {
+        services.AddEafConfiguration();
+        services.AddEafLogging(logger);
+
+        return services;
+    }
+}
