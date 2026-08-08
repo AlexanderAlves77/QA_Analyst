c9686abe365cbddf8ea4b47ba921dc2df16005a0
Alexander Alves <vendas77.rj@gmail.com>
2026-08-08 13:08:16 -0300

    feat(configuration): add environment name constants

---
+++ EnterpriseAutomationFramework/src/Core/Constants/EnvironmentNames.cs
@@ -0,0 +1,12 @@
+﻿namespace EnterpriseAutomationFramework.Core.Constants;
+
+public static class EnvironmentNames
+{
+    public const string Development = "Development";
+
+    public const string QA = "QA";
+
+    public const string Homolog = "Homolog";
+
+    public const string Production = "Production";
+}
