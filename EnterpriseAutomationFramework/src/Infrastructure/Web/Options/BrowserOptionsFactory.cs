10b47416b5721475d57fdc584223ca7149085d01
Alexander Alves <vendas77.rj@gmail.com>
2026-08-15 11:02:31 -0300

    feat(web): add browser options factory

---
+++ EnterpriseAutomationFramework/src/Infrastructure/Web/Options/BrowserOptionsFactory.cs
@@ -0,0 +1,75 @@
+﻿using EnterpriseAutomationFramework.Core.Configuration;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
+namespace EnterpriseAutomationFramework.Infrastructure.Web.Options;
+
+public static class BrowserOptionsFactory
+{
+    public static ChromeOptions CreateChrome(BrowserSettings settings)
+    {
+        var options = new ChromeOptions();
+
+        if (settings.Headless)
+        {
+            options.AddArgument("--headless");
+        }
+
+        if (!string.IsNullOrWhiteSpace(settings.BinaryPath))
+        {
+            options.BinaryLocation = settings.BinaryPath;
+        }
+
+        foreach (var argument in settings.AdditionalArguments)
+        {
+            options.AddArgument(argument);
+        }
+
+        return options;
+    }
+
+    public static EdgeOptions CreateEdge(BrowserSettings settings)
+    {
+        var options = new EdgeOptions();
+
+        if (settings.Headless)
+        {
+            options.AddArgument("--headless");
+        }
+
+        if (!string.IsNullOrWhiteSpace(settings.BinaryPath))
+        {
+            options.BinaryLocation = settings.BinaryPath;
+        }
+
+        foreach (var argument in settings.AdditionalArguments)
+        {
+            options.AddArgument(argument);
+        }
+
+        return options;
+    }
+
+    public static FirefoxOptions CreateFirefox(BrowserSettings settings)
+    {
+        var options = new FirefoxOptions();
+
+        if (settings.Headless)
+        {
+            options.AddArgument("--headless");
+        }
+
+        if (!string.IsNullOrWhiteSpace(settings.BinaryPath))
+        {
+            options.BinaryLocation = settings.BinaryPath;
+        }
+
+        foreach (var argument in settings.AdditionalArguments)
+        {
+            options.AddArgument(argument);
+        }
+
+        return options;
+    }
+}
