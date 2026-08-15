174d193d6e562fdd44ff13bc5209c82bba31a8b1
Alexander Alves <vendas77.rj@gmail.com>
2026-08-15 10:04:55 -0300

    test(browser): validate element locator model

---
+++ EnterpriseAutomationFramework/tests/Tests/Core/Browser/ElementLocatorTests.cs
@@ -0,0 +1,29 @@
+﻿using EnterpriseAutomationFramework.Core.Enums;
+using EnterpriseAutomationFramework.Core.Models;
+
+namespace EnterpriseAutomationFramework.Tests.Core.Browser;
+
+public class ElementLocatorTests
+{
+    [Test]
+    public void Constructor_ShouldCreateElementLocator()
+    {
+        var locator = new ElementLocator(
+            LocatorType.Id,
+            "username"
+        );
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(
+                locator.Type,
+                Is.EqualTo(LocatorType.Id)
+            );
+
+            Assert.That(
+                locator.Value,
+                Is.EqualTo("username")
+            );
+        });
+    }
+}
