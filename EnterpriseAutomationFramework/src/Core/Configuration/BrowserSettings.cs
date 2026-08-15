2fea7a66d2c6667cef1f2f867d738a2de79bcd9c
Alexander Alves <vendas77.rj@gmail.com>
2026-08-15 10:41:06 -0300

    feat(browser): add browser settings configuration

---
+++ EnterpriseAutomationFramework/src/Core/Configuration/BrowserSettings.cs
@@ -0,0 +1,26 @@
+﻿using EnterpriseAutomationFramework.Core.Enums;
+
+namespace EnterpriseAutomationFramework.Core.Configuration;
+
+public sealed class BrowserSettings
+{
+    public const string SectionName = "Browser";
+
+    public BrowserType Type { get; set; } = BrowserType.Chrome;
+
+    public bool Headless { get; set; }
+
+    public int WindowWidth { get; init; } = 1920;
+
+    public int WindowHeight { get; init; } = 1080;
+
+    public int PageLoadTimeoutSeconds { get; init; } = 60;
+
+    public int ScriptTimeoutSeconds { get; init; } = 30;
+
+    public int ImplicitWaitSeconds { get; init; } 
+
+    public string? BinaryPath { get; init; }
+
+    public string[] AdditionalArguments { get; init; } = [];
+}
