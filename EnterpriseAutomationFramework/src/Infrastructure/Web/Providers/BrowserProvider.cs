50d6165d38bab0a84c7cb82376a57ac5984ea6ff
Alexander Alves <vendas77.rj@gmail.com>
2026-08-15 15:42:43 -0300

    feat(web): implement browser provider

---
+++ EnterpriseAutomationFramework/src/Infrastructure/Web/Providers/BrowserProvider.cs
@@ -0,0 +1,33 @@
+﻿using EnterpriseAutomationFramework.Core.Abstractions.Browser;
+using EnterpriseAutomationFramework.Core.Configuration;
+
+namespace EnterpriseAutomationFramework.Infrastructure.Web.Providers;
+
+public sealed class BrowserProvider : IBrowserProvider, IDisposable
+{
+    private readonly IBrowserFactory _browserFactory;
+    private readonly BrowserSettings _settings;
+
+    private IBrowser? _browser;
+
+    public BrowserProvider(IBrowserFactory browserFactory, BrowserSettings settings)
+    {
+        _browserFactory = browserFactory;
+        _settings = settings;
+    }
+
+    public IBrowser Browser
+    {
+        get
+        {
+            _browser ?? = _browserFactory.Create(_settings.Type);
+            return _browser;
+        }
+    }
+
+    public void Dispose()
+    {
+        _browser?.Dispose();
+        _browser = null;
+    }
+}
