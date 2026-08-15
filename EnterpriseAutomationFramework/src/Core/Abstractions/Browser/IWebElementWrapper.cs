42ef8ce031704473725df8aec4bad78551799471
Alexander Alves <vendas77.rj@gmail.com>
2026-08-15 09:39:11 -0300

    feat(browser): add web element abstraction

---
+++ EnterpriseAutomationFramework/src/Core/Abstractions/Browser/IWebElementWrapper.cs
@@ -0,0 +1,18 @@
+﻿namespace EnterpriseAutomationFramework.Core.Abstractions.Browser;
+
+public interface IWebElementWrapper
+{
+    string Text { get; }
+
+    bool IsDisplayed { get; }
+
+    bool IsEnabled { get; }
+
+    void Click();
+
+    void Clear();
+
+    void SendKeys(string text);
+
+    string? GetAttribute(string attributeName);
+}
