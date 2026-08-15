1145bd0a0f185345bbda9a85df95d0d80a901ae1
Alexander Alves <vendas77.rj@gmail.com>
2026-08-15 09:50:12 -0300

    feat(browser): add browser factory abstraction

---
+++ EnterpriseAutomationFramework/src/Core/Abstractions/Browser/IBrowserFactory.cs
@@ -0,0 +1,8 @@
+﻿using EnterpriseAutomationFramework.Core.Enums;
+
+namespace EnterpriseAutomationFramework.Core.Abstractions.Browser;
+
+public interface IBrowserFactory
+{
+    IBrowser Create(BrowserType browserType);
+}
