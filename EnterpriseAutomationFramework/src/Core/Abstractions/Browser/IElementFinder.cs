467361d1d67e55cf5003bae23e398db3500b5b19
Alexander Alves <vendas77.rj@gmail.com>
2026-08-15 09:41:32 -0300

    feat(browser): add element finder abstraction

---
+++ EnterpriseAutomationFramework/src/Core/Abstractions/Browser/IElementFinder.cs
@@ -0,0 +1,10 @@
+﻿using EnterpriseAutomationFramework.Core.Models;
+
+namespace EnterpriseAutomationFramework.Core.Abstractions.Browser;
+
+public interface IElementFinder
+{
+    IWebElementWrapper Find(ElementLocator locator);
+
+    IReadOnlyCollection<IWebElementWrapper> FindAll(ElementLocator locator);
+}
