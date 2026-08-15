d7c11a88ba3ce0b7d8649c9d91cdb1dd4c3b3646
Alexander Alves <vendas77.rj@gmail.com>
2026-08-15 15:12:01 -0300

    feat(web): implement Selenium element finder

---
+++ EnterpriseAutomationFramework/src/Infrastructure/Web/Elements/SeleniumElementFinder.cs
@@ -0,0 +1,34 @@
+﻿using EnterpriseAutomationFramework.Core.Abstractions.Browser;
+using EnterpriseAutomationFramework.Core.Models;
+using OpenQA.Selenium;
+
+namespace EnterpriseAutomationFramework.Infrastructure.Web.Elements;
+
+public sealed class SeleniumElementFinder : IElementFinder
+{
+    private readonly IWebDriver _driver;
+
+    public SeleniumElementFinder(IWebDriver driver)
+    {
+        _driver = driver
+            ?? throw new ArgumentNullException(nameof(driver));
+    }
+
+    public IWebElementWrapper Find(ElementLocator locator)
+    {
+        var by = SeleniumLocatorMapper.ToBy(locator);
+
+        var element = _driver.FindElement(by);
+
+        return new SeleniumWebElementWrapper(element);
+    }
+
+    public IReadOnlyCollection<IWebElementWrapper> FindAll(ElementLocator locator)
+    {
+        var by = SeleniumLocatorMapper.ToBy(locator);
+
+        return _driver.FindElements(by)
+            .Select(element => (IWebElementWrapper)
+                new SeleniumWebElementWrapper(element)).ToArray();
+    }
+}
