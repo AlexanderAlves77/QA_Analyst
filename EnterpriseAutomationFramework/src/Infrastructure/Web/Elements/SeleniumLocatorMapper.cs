d6fb2df5dbd592d77b70782f1e8a7ffefaf857f1
Alexander Alves <vendas77.rj@gmail.com>
2026-08-15 14:50:50 -0300

    feat(web): implement Selenium locator mapper

---
+++ EnterpriseAutomationFramework/src/Infrastructure/Web/Elements/SeleniumLocatorMapper.cs
@@ -0,0 +1,40 @@
+﻿using EnterpriseAutomationFramework.Core.Enums;
+using EnterpriseAutomationFramework.Core.Exceptions;
+using EnterpriseAutomationFramework.Core.Models;
+using OpenQA.Selenium;
+
+namespace EnterpriseAutomationFramework.Infrastructure.Web.Elements;
+
+public static class SeleniumLocatorMapper
+{
+    public static By ToBy(ElementLocator locator)
+    {
+        ArgumentNullException.ThrowIfNull(locator); 
+
+        if (string.IsNullOrWhiteSpace(locator.Value))
+        {
+            throw new FrameworkException("Element locator value cannot be empty.");
+        }
+
+        return locator.Type switch
+        {
+            LocatorType.Id => By.Id(locator.Value),
+
+            LocatorType.Name => By.Name(locator.Value),
+
+            LocatorType.CssSelector => By.CssSelector(locator.Value),
+
+            LocatorType.XPath => By.XPath(locator.Value),
+
+            LocatorType.ClassName => By.ClassName(locator.Value),
+
+            LocatorType.TagName => By.TagName(locator.Value),
+
+            LocatorType.LinkText => By.LinkText(locator.Value),
+
+            LocatorType.PartialLinkText => By.PartialLinkText(locator.Value),
+
+            _ => throw new FrameworkException($"Unsupported locator type: {locator.Type}.")
+        };
+    }
+}
