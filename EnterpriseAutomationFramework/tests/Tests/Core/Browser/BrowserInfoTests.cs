f48e84681462984299ed54ab2741b53654ce321d
Alexander Alves <vendas77.rj@gmail.com>
2026-08-15 10:01:25 -0300

    test(browser): validate browser information model

---
+++ EnterpriseAutomationFramework/tests/Tests/Core/Browser/BrowserInfoTests.cs
@@ -0,0 +1,35 @@
+﻿using EnterpriseAutomationFramework.Core.Enums;
+using EnterpriseAutomationFramework.Core.Models;
+
+namespace EnterpriseAutomationFramework.Tests.Core.Browser;
+
+public class BrowserInfoTests
+{
+    [Test]
+    public void Constructor_ShouldCreateBrowserInfo()
+    {
+        var browserInfo = new BrowserInfo(
+            BrowserType.Chrome,
+            "Google Chrome",
+            "139"
+        );
+
+        Assert.Multiple(() => 
+        {
+            Assert.That(
+                browserInfo.Type,
+                Is.EqualTo(BrowserType.Chrome)
+            );
+
+            Assert.That(
+                browserInfo.Name,
+                Is.EqualTo("Google Chrome")
+            );
+
+            Assert.That(
+                browserInfo.Version,
+                Is.EqualTo("139")
+            );
+        });
+    }
+}
