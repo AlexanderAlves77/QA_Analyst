96e55e6191aa047657e1fb67b1b169fcabe048bd
Alexander Alves <vendas77.rj@gmail.com>
2026-08-15 14:56:37 -0300

    test(web): validate Selenium locator mapping

---
+++ EnterpriseAutomationFramework/tests/Tests/Infrastructure/Web/SeleniumLocatorMapperTests.cs
@@ -0,0 +1,40 @@
+﻿using EnterpriseAutomationFramework.Core.Enums;
+using EnterpriseAutomationFramework.Core.Models;
+using EnterpriseAutomationFramework.Infrastructure.Web.Elements;
+
+namespace EnterpriseAutomationFramework.Tests.Infrastructure.Web;
+
+public class SeleniumLocatorMapperTests
+{
+    [Test]
+    public void ToBy_ShouldMapIdLocator()
+    {
+        var locator = new ElementLocator(LocatorType.Id, "username");
+
+        var by = SeleniumLocatorMapper.ToBy(locator);
+
+        Assert.That(by.ToString(), Does.Contain("username"));
+    }
+
+    [Test]
+    public void ToBy_ShouldMapCssSelectorLocator()
+    {
+        var locator = new ElementLocator(
+            LocatorType.CssSelector, "[data-testid='login']");
+
+        var by = SeleniumLocatorMapper.ToBy(locator);
+
+        Assert.That(by.ToString(), Does.Contain("[data-testid='login']"));
+    }
+
+    [Test]
+    public void ToBy_ShouldMapXPathLocator()
+    {
+        var locator = new ElementLocator(
+            LocatorType.CssSelector, "//button[@type='submit']");
+
+        var by = SeleniumLocatorMapper.ToBy(locator);
+
+        Assert.That(by.ToString(), Does.Contain("//button[@type='submit']"));
+    }
+}
