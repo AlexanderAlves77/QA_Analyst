5a75d1a7dda40635fa0d28e0dfc202f47aef45a5
Alexander Alves <vendas77.rj@gmail.com>
2026-08-15 09:44:03 -0300

    feat(browser): add browser abstraction

---
+++ EnterpriseAutomationFramework/src/Core/Abstractions/Browser/IBrowser.cs
@@ -0,0 +1,18 @@
+﻿using EnterpriseAutomationFramework.Core.Models;
+
+namespace EnterpriseAutomationFramework.Core.Abstractions.Browser;
+
+public interface IBrowser : IDisposable
+{
+    BrowserInfo Info { get; }
+
+    INavigator Navigator { get; }
+
+    IElementFinder Elements { get; }
+
+    string CurrentUrl { get; }
+
+    string Title { get; }
+
+    void Close();
+}
