3e9be48e1feb95a763ea958cee1b3603b1674c02
Alexander Alves <vendas77.rj@gmail.com>
2026-08-22 09:13:56 -0300

    feat(actions): add element actions abstraction

---
+++ EnterpriseAutomationFramework/src/Core/Abstractions/Browser/IElementActions.cs
@@ -0,0 +1,16 @@
+﻿using EnterpriseAutomationFramework.Core.Models;
+
+namespace EnterpriseAutomationFramework.Core.Abstractions.Browser;
+
+public interface IElementActions
+{
+    void Click(ElementLocator locator);
+
+    void Type(ElementLocator locator, string text, bool clearFirst = true);
+
+    void Clear(ElementLocator locator);
+
+    string GetText(ElementLocator locator);
+
+    bool IsVisible(ElementLocator locator);
+}
