eb3fbff178a4b466fb8dc86247d57df042b92700
Alexander Alves <vendas77.rj@gmail.com>
2026-08-22 08:53:13 -0300

    feat(wait): add wait strategy abstraction

---
+++ EnterpriseAutomationFramework/src/Core/Abstractions/Browser/IWaitStrategy.cs
@@ -0,0 +1,14 @@
+﻿using EnterpriseAutomationFramework.Core.Models;
+
+namespace EnterpriseAutomationFramework.Core.Abstractions.Browser;
+
+public interface IWaitStrategy
+{
+    IWebElementWrapper UntilPresent(ElementLocator locator);
+
+    IWebElementWrapper UntilVisible(ElementLocator locator);
+
+    IWebElementWrapper UntilClickable(ElementLocator locator);
+
+    bool UntilInvisible(ElementLocator locator);
+}
