40a988327b98b40af066bfd12952542730b3bfcb
Alexander Alves <vendas77.rj@gmail.com>
2026-08-22 08:45:39 -0300

    feat(wait): add wait settings configuration

---
+++ EnterpriseAutomationFramework/src/Core/Configuration/WaitSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace EnterpriseAutomationFramework.Core.Configuration;
+
+public sealed class WaitSettings
+{
+    public const string SectionName = "Wait";
+
+    public int DefaultTimeoutSeconds { get; init; } = 15;
+
+    public int PollingIntervalMilliseconds { get; init; } = 500;
+}
