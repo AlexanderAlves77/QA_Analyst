c42a1d508638cbed61752748c3978511d225ab57
Alexander Alves <vendas77.rj@gmail.com>
2026-08-22 09:21:19 -0300

    feat(actions): implement Selenium element actions

---
+++ EnterpriseAutomationFramework/src/Infrastructure/Web/Actions/SeleniumElementActions.cs
@@ -0,0 +1,60 @@
+﻿using EnterpriseAutomationFramework.Core.Abstractions.Browser;
+using EnterpriseAutomationFramework.Core.Models;
+
+namespace EnterpriseAutomationFramework.Infrastructure.Web.Actions;
+
+public sealed class SeleniumElementActions
+{
+    private readonly IWaitStrategy _waitStrategy;
+
+    public SeleniumElementActions(IWaitStrategy waitStrategy)
+    {
+        _waitStrategy = waitStrategy 
+            ?? throw new ArgumentNullException(nameof(waitStrategy));
+    }
+
+    public void Click(ElementLocator locator)
+    {
+        var element = _waitStrategy.UntilClickable(locator);
+        element.Click();
+    }
+
+    public void Type(ElementLocator locator, string text, bool clearFirst = true)
+    {
+        ArgumentNullException.ThrowIfNull(text);
+
+        var element = _waitStrategy.UntilVisible(locator);
+
+        if (clearFirst)
+        {
+            element.Clear();
+        }
+
+        element.SendKeys(text);
+    }
+
+    public void Clear(ElementLocator locator)
+    {
+        var element = _waitStrategy.UntilVisible(locator);
+        element.Clear();
+    }
+
+    public string GetText(ElementLocator locator)
+    {
+        var element = _waitStrategy.UntilVisible(locator);
+        return element.Text;
+    }
+
+    public bool IsVisible(ElementLocator locator)
+    {
+        try
+        {
+            _waitStrategy.UntilVisible(locator);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
