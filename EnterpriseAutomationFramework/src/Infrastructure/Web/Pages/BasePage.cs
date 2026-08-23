c0609090efd0985cf69aeee7dc885192a6ab7494
Alexander Alves <vendas77.rj@gmail.com>
2026-08-23 11:52:30 -0300

    feat(pages): add base page abstraction

---
+++ EnterpriseAutomationFramework/src/Infrastructure/Web/Pages/BasePage.cs
@@ -0,0 +1,13 @@
+﻿using EnterpriseAutomationFramework.Core.Abstractions.Browser;
+
+namespace EnterpriseAutomationFramework.Infrastructure.Web.Pages;
+
+public abstract class BasePage
+{
+    protected IBrowser Browser { get; }
+
+    protected BasePage(IBrowser browser)
+    {
+        Browser = browser ?? throw new ArgumentNullException(nameof(browser));
+    }
+}
