8cd57c36cd5e0531292c0ee685a4e6aa23b6be11
Alexander Alves <vendas77.rj@gmail.com>
2026-08-23 12:00:22 -0300

    feat(pages): add login page object

---
+++ EnterpriseAutomationFramework/src/Infrastructure/Web/Pages/LoginPage.cs
@@ -0,0 +1,47 @@
+﻿using EnterpriseAutomationFramework.Core.Abstractions.Browser;
+using EnterpriseAutomationFramework.Core.Enums;
+using EnterpriseAutomationFramework.Core.Models;
+
+namespace EnterpriseAutomationFramework.Infrastructure.Web.Pages;
+
+public sealed class LoginPage : BasePage
+{
+    private readonly ElementLocator _usernameInput = new(LocatorType.Id, "username");
+
+    private readonly ElementLocator _passwordInput = new(LocatorType.Id, "password");
+
+    private readonly ElementLocator _loginButton = 
+        new(LocatorType.CssSelector, "button[type='submit']");
+
+    public LoginPage(IBrowser browser) : base(browser)
+    { }
+
+    public void GoTo(string url)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(nameof(url));
+
+        Browser.Navigator.GoTo(url);    
+    }
+
+    public void EnterUsername(string username)
+    {
+        Browser.Actions.Type(_usernameInput, username);
+    }
+
+    public void EnterPassword(string password)
+    {
+        Browser.Actions.Type(_passwordInput, password);
+    }
+
+    public void ClickLogin()
+    {
+        Browser.Actions.Click(_loginButton);
+    }
+
+    public void Login(string username, string password)
+    {
+        EnterUsername(username);
+        EnterPassword(password);
+        ClickLogin();
+    }
+}
