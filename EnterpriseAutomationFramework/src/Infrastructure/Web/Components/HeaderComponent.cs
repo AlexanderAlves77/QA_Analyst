2e67eade9ade39ca029c58fd1ce6016a0cafff5b
Alexander Alves <vendas77.rj@gmail.com>
2026-08-23 12:05:17 -0300

    feat(components): add header component

---
+++ EnterpriseAutomationFramework/src/Infrastructure/Web/Components/HeaderComponent.cs
@@ -0,0 +1,26 @@
+﻿using EnterpriseAutomationFramework.Core.Abstractions.Browser;
+using EnterpriseAutomationFramework.Core.Enums;
+using EnterpriseAutomationFramework.Core.Models;
+
+namespace EnterpriseAutomationFramework.Infrastructure.Web.Components;
+
+public sealed class HeaderComponent : BaseComponent
+{
+    private readonly ElementLocator _profileButton = new(LocatorType.Id, "profile");
+
+    private readonly ElementLocator _logoutButton = new(LocatorType.Id, "logout");
+
+    public HeaderComponent(IBrowser browser) : base(browser)
+    { }
+
+    public void OpenProfile()
+    {
+        Browser.Actions.Click(_profileButton);
+    }
+
+    public void Logout()
+    {
+        Browser.Actions.Click(_profileButton);
+        Browser.Actions.Click(_logoutButton);
+    }
+}
