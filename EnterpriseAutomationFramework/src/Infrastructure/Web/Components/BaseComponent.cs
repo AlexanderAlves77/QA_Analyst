d5c4c187c93491ae540dd48d3cdc2c42416e784e
Alexander Alves <vendas77.rj@gmail.com>
2026-08-23 12:02:00 -0300

    feat(components): add base component abstraction

---
+++ EnterpriseAutomationFramework/src/Infrastructure/Web/Components/BaseComponent.cs
@@ -0,0 +1,13 @@
+﻿using EnterpriseAutomationFramework.Core.Abstractions.Browser;
+
+namespace EnterpriseAutomationFramework.Infrastructure.Web.Components;
+
+public abstract class BaseComponent
+{
+    protected IBrowser Browser { get; }
+
+    protected BaseComponent(IBrowser browser)
+    {
+        Browser = browser ?? throw new ArgumentNullException(nameof(browser));
+    }
+}
